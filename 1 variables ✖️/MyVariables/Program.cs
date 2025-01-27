dce2d276df2f7f54f7315ab280e0eb543fac6197
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-27 16:11:51 +0530

    Learn: TypeCasting.

---
+++ 1 variables ✖️/MyVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x; //decration
+x = 123; //Initialization
+
+int y = 321; // decration + Initialization
+
+int z = x + y;
+
+//Console.WriteLine(x);
+//Console.WriteLine(y);
+//Console.WriteLine(z);
+
+int age = 21;// whole number
+double height = 300.5;// Decimal Number 
+bool alive = true;//T or F
+char symbol = '@';//use single qoute
+String name = "alkahaf";
+
+const double pi = 3.1412345;
+
+
+Console.WriteLine(pi);
+Console.WriteLine("Hello "+ name);
+Console.WriteLine("Your Age is " + age);
+Console.WriteLine("Your Height is " + height + "cm");
+Console.WriteLine("Are you alive "+alive);
+Console.WriteLine("your symole is "+ symbol);
+
+String userName = symbol + name;
+Console.WriteLine("your username is "+ userName);
+
+Console.ReadKey();
