402ff4410b7657ef063caca9b31df19c578fcd1d
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-27 13:30:14 +0530

    first commit

---
+++ output 💬/MyFirstProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+Console.WriteLine("Hello World!");
+
+Console.Beep();
+
+
+//Escape Sequence
+Console.WriteLine("\aBroCode");
+Console.WriteLine("\bBroCode\b");
+Console.WriteLine("\fBroCode");
+Console.WriteLine("BroCode\n");
+Console.WriteLine("\rBroCode");
+Console.WriteLine("\tBroCode");
+Console.WriteLine("\vBroCode");
+Console.WriteLine("\'BroCode");
+Console.WriteLine("\"BroCode");
+Console.WriteLine("\\BroCode");
+Console.ReadKey();
---
+++ variables ✖️/MyVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x; //decration
+x = 123; //Initialization
+
+int y = 321; // decration + Initialization
+
+int z = x + y ;
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadKey();
