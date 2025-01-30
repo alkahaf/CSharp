584c0be7737bee7cf2821b06e04fd4b7ca95935c
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-30 11:23:18 +0530

    [ADD]:[NEW FILE]

---
+++ user input ⌨️/userInput/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Your Name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter your Age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your Name is: " + name);
+Console.WriteLine("Your Age is: " + age);
