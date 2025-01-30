6971c3192e157023472a255e576ee2632bd081a8
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-30 12:54:01 +0530

    RandomNumber

---
+++ 7 random numbers 🎲/RandomNum/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Random Numbers :");
+
+Random random = new Random();
+
+int num1 = random.Next();// if i not define range it generate 10 digit number.
+int num2 = random.Next(0,6);// if i not define range it generate 10 digit number.
+
+double num3 = random.NextDouble();
+
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Console.WriteLine(num3);
+Console.ReadLine();
