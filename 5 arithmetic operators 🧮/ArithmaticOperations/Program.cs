0483a0621b52fa63c9578244612ea5602a3d28b1
AlkahafM <alkahafmukadam2@gmail.com>
2025-01-30 12:19:26 +0530

    MathClasses

---
+++ 5 arithmetic operators 🧮/ArithmaticOperations/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Arithmatic Operations");
+
+int friends = 10;
+
+friends = friends + 1; //standard way 
+friends += 1;//shortcut
+friends++;// use in loop (increament by 1 only).
+Console.WriteLine(friends);
---
+++ 6 Math class 📏/MathsClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Maths Classes");
+double x = 12.9;
+double y = 12.8;
+
+//double a = Math.Pow(x,2);
+//double b = Math.Sqrt(x);
+//double c = Math.Abs(x); I/P= -3
+//double d = Math.Round(x); round and ceiling methods are same
+//double e =Math.Ceiling(x);
+//double  f = Math.Floor(x);
+//double g = Math.Max(x,y);
+double g = Math.Min(x,y);
+
+
+Console.WriteLine(g);
+
+Console.ReadLine();
