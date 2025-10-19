685690c03ea6dd395d5b5f06e58f6ac62e5939dd
Al-kahaf Mukadam <alkahafmukadam2@gmail.com>
2025-10-19 10:19:55 +0530

    While Loops

---
+++ while loops ♾️/while loops ♾️/Program.cs
@@ -0,0 +1,15 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Your Name: ");
+        string name = Console.ReadLine();
+        while(name =="")
+        {
+            Console.Write("Enter Your Name: ");
+            name = Console.ReadLine();
+        }
+        Console.WriteLine("hello " + name);
+        Console.ReadKey();
+    }
+}
