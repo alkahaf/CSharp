e4c3df87e47e5409021b5d25a44aa2cee37b414b
Al-kahaf Mukadam <alkahafmukadam2@gmail.com>
2025-10-19 20:03:15 +0530

    nested loops

---
+++ 17 nested loops ➿/nested loops ➿/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program {
+
+    static void Main(String[] args)
+    {
+        Console.Write("Enter number of rows:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter number of columns:");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter special sign:");
+        String sign = Console.ReadLine();
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= columns; j++)
+            {
+                Console.Write(sign);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
